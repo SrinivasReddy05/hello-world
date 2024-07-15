730e3ce4f4de37211ebac97f4f56f2245a8f0513
sreenu <peramsrinivasareddy789@gmail.com>
2024-07-15 14:52:17 +0530

    Swapping variables in pycharm .and the file has been added and commited to the GITHUB account

---
+++ swap_variables.py
@@ -0,0 +1,16 @@
+# Swap using a temporary variable
+
+# Define the numbers
+a = 5
+b = 10
+
+# Print original numbers
+print(f"Original numbers: a = {a}, b = {b}")
+
+# Swap
+temp = a
+a = b
+b = temp
+
+# Print swapped numbers
+print(f"Swapped numbers: a = {a}, b = {b}")
